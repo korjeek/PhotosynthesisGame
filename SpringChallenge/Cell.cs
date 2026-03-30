5061c5a5cd1e60d81737f3eb20055dfb1d7a193f
korjeek <olegka823@gmail.com>
2026-03-30 14:42:51 +0500

    Initial commit

---
+++ SpringChallenge/Cell.cs
@@ -0,0 +1,15 @@
+﻿namespace SpringChallenge;
+
+public class Cell
+{
+    public int index;
+    public int richness;
+    public int[] neighbours;
+
+    public Cell(int index, int richness, int[] neighbours)
+    {
+        this.index = index;
+        this.richness = richness;
+        this.neighbours = neighbours;
+    }
+}
---
+++ SpringChallenge/Game.cs
@@ -0,0 +1,40 @@
+﻿namespace SpringChallenge;
+
+public class Game
+{
+    public int day;
+    public int nutrients;
+    public List<Cell> board;
+    public List<Action> possibleActions;
+    public List<Tree> trees;
+    public int mySun, opponentSun;
+    public int myScore, opponentScore;
+    public bool opponentIsWaiting;
+
+    public Game()
+    {
+        board = [];
+        possibleActions = [];
+        trees = [];
+    }
+
+    public Action GetNextAction()
+    {
+        var myTreesIdx = trees
+            .Where(t => t.isMine)
+            .Select(t => t.cellIndex)
+            .ToHashSet();
+
+        var bestCell = -1;
+        var bestRichness = int.MinValue;
+        foreach (var cell in board.Where(cell => myTreesIdx.Contains(cell.index) && cell.richness > bestRichness))
+        {
+            bestCell = cell.index;
+            bestRichness = cell.richness;
+        }
+
+        return bestCell == -1 ? 
+            new Action(Action.WAIT) : 
+            new Action(Action.COMPLETE, bestCell);
+    }
+}
