b4f4aa60e26146ce5782b998563d4cd63b30647c
korjeek <olegka823@gmail.com>
2026-03-30 15:31:37 +0500

    feat: update baseline for new league
    Signed-off-by: korjeek <olegka823@gmail.com>

--- SpringChallenge/Game.cs
+++ SpringChallenge/Game.cs
@@ -20,21 +20,60 @@ public class Game
 
     public Action GetNextAction()
     {
-        var myTreesIdx = trees
-            .Where(t => t.isMine)
-            .Select(t => t.cellIndex)
-            .ToHashSet();
+        var mySize2 = trees.Count(t => t is { isMine: true, size: 2 });
+        var mySize3 = trees.Count(t => t is { isMine: true, size: 3 });
+        
+        var completeCandidates = trees
+            .Where(t => t is { isMine: true, size: 3, isDormant: false })
+            .Select(t => new { t.cellIndex, Bonus = GetBonus(t.cellIndex) })
+            .ToList();
 
-        var bestCell = -1;
-        var bestRichness = int.MinValue;
-        foreach (var cell in board.Where(cell => myTreesIdx.Contains(cell.index) && cell.richness > bestRichness))
+        if (completeCandidates.Count != 0 && mySun >= 4)
         {
-            bestCell = cell.index;
-            bestRichness = cell.richness;
+            var best = completeCandidates.OrderByDescending(c => c.Bonus).First();
+            return new Action(Action.COMPLETE, best.cellIndex);
         }
+        
+        var costGrow23 = 7 + mySize3;
+        var grow23Candidates = trees
+            .Where(t => t is { isMine: true, size: 2, isDormant: false })
+            .Select(t => new { t.cellIndex, Richness = GetRichness(t.cellIndex) })
+            .ToList();
 
-        return bestCell == -1 ? 
-            new Action(Action.WAIT) : 
-            new Action(Action.COMPLETE, bestCell);
+        if (grow23Candidates.Count != 0 && mySun >= costGrow23)
+        {
+            var best = grow23Candidates.OrderByDescending(c => c.Richness).First();
+            return new Action(Action.GROW, best.cellIndex);
+        }
+        
+        var costGrow12 = 3 + mySize2;
+        var grow12Candidates = trees
+            .Where(t => t.isMine && t is { size: 1, isDormant: false })
+            .Select(t => new { t.cellIndex, Richness = GetRichness(t.cellIndex) })
+            .ToList();
+
+        if (grow12Candidates.Count != 0 && mySun >= costGrow12)
+        {
+            var best = grow12Candidates.OrderByDescending(c => c.Richness).First();
+            return new Action(Action.GROW, best.cellIndex);
+        }
+        
+        return new Action(Action.WAIT);
+    }
+    
+    private int GetBonus(int cellIndex)
+    {
+        var richness = GetRichness(cellIndex);
+        return richness switch
+        {
+            3 => 4,
+            2 => 2,
+            _ => 0
+        };
+    }
+    
+    private int GetRichness(int cellIndex)
+    {
+        return board.First(c => c.index == cellIndex).richness;
     }
 }
